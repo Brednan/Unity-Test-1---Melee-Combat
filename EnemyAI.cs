4f493ecd661c9642116af4ec766aeb8d60432bd0
Brendan Caldwell <40771777+Brednan@users.noreply.github.com>
2019-08-15 16:31:17 -0700

    Add files via upload

---
+++ EnemyAI.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField]
+    Animator enemyAnim;
+
+    Transform player;
+    Player _playerScript;
+
+    private bool canAttack;
+
+    private float speed;
+    private float playerDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canAttack = true;
+        playerDistance = 34f;
+        speed = 1.8f;
+        GameObject target = GameObject.FindGameObjectWithTag("Player");
+        player = target.transform;
+        _playerScript = GameObject.FindGameObjectWithTag("Player").GetComponent<Player>();
+        _playerScript.enemyHasBeenHit = false;
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerAttack();
+        DestroyThis();
+    }
+    void PlayerAttack()
+    {
+        transform.LookAt(player);
+        if(Vector3.Distance(transform.position, player.position) > playerDistance)
+        {
+            if(canAttack == true)
+            {
+                transform.Translate(Vector3.forward * speed);
+                enemyAnim.Play("Run");
+            }
+        }
+        else if(Vector3.Distance(transform.position, player.position) < playerDistance)
+        {
+            if(canAttack == true)
+            {
+                StartCoroutine("EnemyAttackCoolDown");
+                StartCoroutine("EnemyAttackRoutine");
+            }
+        }
+        else
+        {
+            enemyAnim.Play("Idle");
+        }
+
+    }
+    public void DestroyThis()
+    {
+        if(_playerScript.enemyHasBeenHit == true)
+        {
+            GameObject.Destroy(this.gameObject);
+        }
+
+    }
+    private IEnumerator EnemyAttackCoolDown()
+    {
+        canAttack = false;
+        yield return new WaitForSeconds(enemyAnim.GetCurrentAnimatorClipInfo(0).Length);
+        canAttack = true;
+        yield break;
+    }
+    private IEnumerator EnemyAttackRoutine()
+    {
+
+        yield return new WaitForSeconds(0.3f);
+        enemyAnim.Play("Attack");
+        Debug.Log("EnemyHit");
+        _playerScript.PlayerDeath();
+        yield break;
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Animator anim;
+
+    [SerializeField]
+    private float speed;
+    private float attackSpeed;
+    private Vector3 _spawnPoint;
+
+    [SerializeField]
+    Rigidbody rb;
+
+
+    [SerializeField]
+    RaycastHit hit;
+    Camera cam;
+
+    private EnemyAI _enemyScript;
+    [SerializeField]
+    GameObject _enemyPrefab;
+
+    private Transform _enemy;
+    public bool enemyHasBeenHit;
+
+    private bool attack;
+    // Start is called before the first frame update
+    void Start()
+    {
+        attack = false;
+        speed = 2.0f;
+        attackSpeed = 1.0f;
+        cam = Camera.main;
+        GameObject _enemyPrefab = GameObject.FindGameObjectWithTag("Enemy");
+        _enemy = _enemyPrefab.transform;
+        _enemyScript = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyAI>();
+        _spawnPoint = new Vector3(-16608, -6430.6f, -43578f);
+    }
+        // Update is called once per frame
+        void Update()
+        {
+
+        CursorLock();
+        Ray ray = new Ray(transform.position, transform.rotation * Vector3.forward);
+        if (!attack)
+            {
+            if (Input.GetMouseButtonDown(0) && Input.GetKey(KeyCode.W))
+            {
+                StartCoroutine("AttackRoutine");
+                if (attack)
+                {
+                    anim.Play("Attack");
+                    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+                    {
+                        if(hit.collider.tag == "Enemy")
+                        {
+                            Debug.Log("Hit");
+                            Attack();
+                        }
+
+                    }
+
+                }
+            }
+            else if (Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine("AttackRoutine");
+                if (attack)
+                {
+                    anim.Play("Attack");
+                    if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity))
+                    {
+                        if (hit.collider.tag == "Enemy")
+                        {
+                            Debug.Log("Hit");
+                            Attack();
+                        }
+                    }
+                }
+            }
+
+                else if (Input.GetKey(KeyCode.W))
+                {
+                    Run();
+                }
+                else
+                {
+                    anim.Play("Idle");
+                }
+            }
+            LookAround();
+
+        }
+         private void Run()
+        {
+            anim.Play("Run");
+            transform.Translate(Vector3.forward * speed);
+        }
+         private void Attack()
+        {
+        StartCoroutine("EnemySpawnRoutine");
+        }
+        IEnumerator AttackRoutine()
+        {
+            attack = true;
+            yield return new WaitForSeconds(anim.GetCurrentAnimatorClipInfo(0).Length);
+            attack = false;
+            yield break;
+        }
+         private void LookAround()
+        {
+            float _mouseX = Input.GetAxis("Mouse X");
+
+            float _mouseY = Input.GetAxis("Mouse Y");
+
+            transform.localEulerAngles = new Vector3(transform.localEulerAngles.x + _mouseY, transform.localEulerAngles.y + _mouseX, transform.localEulerAngles.z);
+        }
+    private void CursorLock()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+    IEnumerator EnemySpawnRoutine()
+    {
+        enemyHasBeenHit = true;
+        yield return new WaitForSeconds(1.0f);
+        enemyHasBeenHit = false;
+        Instantiate(_enemyPrefab, new Vector3(-16526, -6432, -43730), Quaternion.identity);
+        yield break;
+    }
+    public void PlayerDeath()
+    {
+        transform.position = _spawnPoint;
+    }
+}
